4a9fdce7754a90bf62ac7b59fd68c454f0068027
HelgiL <helgilebed@gmail.com>
2025-10-11 13:59:00 +0300

    complete lab2

--- lab2/script2.r
+++ lab2/script2.r
@@ -1,25 +1,42 @@
 library(dplyr)
-print(starwars %>% nrow())
 
-print(starwars %>% ncol())
-print(starwars %>% glimpse())
-print(starwars %>% distinct(species), n=38)
-print(starwars %>% filter(height == max(height, na.rm = TRUE)))
-print(starwars %>% filter(height<170), n=22)
+#print(starwars %>% nrow())
+
+#print(starwars %>% ncol())
+
+#print(starwars %>% glimpse())
+
+#nrow(starwars %>% distinct(species))
+
+starwars %>% filter(height == max(height, na.rm = TRUE)) %>%  select(name)
+
+
+starwars %>% filter(height<170)
+
 starwars %>%
   filter(!is.na(height), !is.na(mass)) %>%
   mutate(BMI = mass / (height/100)^2) %>%
   select(name, height, mass, BMI) %>%
   print(n=59)
 
+starwars %>%
   filter(!is.na(height), !is.na(mass)) %>%
-  mutate(
-    BMI = mass / (height/100)^2,
-    BMI_category = case_when(
-      BMI < 18.5 ~ "Underweight",
-      BMI < 25 ~ "Normal",
-      BMI < 30 ~ "Overweight",
-      TRUE ~ "Obese"
-    )
-  ) %>%
-  count(BMI_category, sort = TRUE)
+  mutate(elongation = mass / (height)) %>%
+  select(name, elongation) %>%
+  arrange(desc(elongation)) %>%
+  print(n=59)
+
+starwars %>%
+  filter(!is.na(birth_year)) %>%
+  mutate(age = 100 + birth_year) %>% 
+  summarise(mean_age = mean(age))
+
+
+starwars %>%
+count(eye_color, sort = TRUE) %>%
+slice(1) %>%
+pull(eye_color)
+
+starwars %>%
+mutate(str_length = nchar(as.character(name))) %>%
+summarise(avg_length = mean(str_length, na.rm = TRUE))
